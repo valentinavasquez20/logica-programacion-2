bacfe8be76371bddb88926ebae03a095e492d323
valentinavasquez20 <155574285+valentinavasquez20@users.noreply.github.com>
2024-01-12 09:46:40 -0300

    add document

---
+++ vale.js
@@ -0,0 +1,21 @@
+const celcius = prompt("Ingrede los grados celcius: ")
+console.log(celcius)
+
+function fahrenheit() {
+  conv1 = (1.8 * celcius) + 32
+  return conv1
+}
+
+function kelvin() {
+  conv2 = celcius + 273.5
+  return conv2
+}
+
+console.log("Los grados celcius a fahrenheit es: ", fahrenheit())
+
+document.write("Los grados celcius a Fahrenheit es: ",fahrenheit());
+document.write("Los grados celcius a Kelvin es: ",kelvin());
+result.innerHTML = `
+<p>Los grados celcius a fahrenheit es: ${fahrenheit()}</p>
+<p>Los grados celcius a kelvin es: ${kelvin}</p>
+`;
